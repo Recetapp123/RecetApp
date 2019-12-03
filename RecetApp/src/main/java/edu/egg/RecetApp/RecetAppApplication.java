d64c874693d286a8185d1ab00116bd8df19cabaf
fabriziop98 <58437892+fabriziop98@users.noreply.github.com>
2019-12-03 18:13:27 -0300

    Version LIMPIA RecetApp
    en blanco.

---
+++ RecetApp/src/main/java/edu/egg/RecetApp/RecetAppApplication.java
@@ -0,0 +1,13 @@
+package edu.egg.RecetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetAppApplication.class, args);
+	}
+
+}
