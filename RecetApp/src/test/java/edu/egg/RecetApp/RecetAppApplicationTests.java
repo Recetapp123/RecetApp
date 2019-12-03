ebecd2af7059ecba454f22cdd0ea6d2497dda796
Santiago Menéndez <menendezbst@gmail.com>
2019-12-03 18:16:57 -0300

    Revert "Revert "Merge pull request #2 from Recetapp123/Back-End""
    This reverts commit 12bf3c69dd38b51a916035730232e1d14605b084.

---
+++ RecetApp/src/test/java/edu/egg/RecetApp/RecetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.egg.RecetApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
