e0d970ff1555e5a114b1a2dff7257afcb06dfe55
Fabrizio <fabriziop@live.com.ar>
2019-12-16 19:43:50 -0300

    Listado se importa con foto.
    Falta mejorar el estilo

--- RecetApp/src/main/java/edu/egg/RecetApp/Controladores/PrincipalControlador.java
+++ RecetApp/src/main/java/edu/egg/RecetApp/Controladores/PrincipalControlador.java
@@ -1,21 +1,62 @@
 package edu.egg.RecetApp.Controladores;
 
+import edu.egg.RecetApp.Entidades.Ingrediente;
+import edu.egg.RecetApp.Servicios.IngredienteServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/inicio")
 public class PrincipalControlador {
+
+    @Autowired
+    private IngredienteServicio ingredienteServicio;
+    
     @GetMapping("/")
-    public String iniciosion(){
+    public String iniciosion() {
         return "indexNoLogueado";
     }
-    
+
     @GetMapping("/indexLogueado")
-    public String inicio(Model model){
+    public String inicio(Model model) {
         model.addAttribute("usuario", null);
         return "indexLogueado";
+    }
+
+    @GetMapping("/buscar")
+    public String buscador(@RequestParam(required = false) String q, @RequestParam(required = false) String error, ModelMap modelo) {
+        List<Ingrediente> ingredientes;
+        if (q != null) {
+            ingredientes = ingredienteServicio.buscarIngredientes(q);
+        }else{
+            ingredientes = ingredienteServicio.buscarIngredientes();
+        }
+        modelo.put("q",q);
+        modelo.put("ingredientes", ingredientes);
+        modelo.put("error",error);
+        return "indexNoLogueado";
+    }
+    @GetMapping(value = "/image/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable(value = "id") String id) {
+
+        Ingrediente ingrediente = null;
+        ingrediente = ingredienteServicio.buscarPorId(id);
+
+        byte[] foto = ingrediente.getFoto().getContenido();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        return new ResponseEntity<byte[]>(foto, headers, HttpStatus.OK);
+
     }
 }
--- RecetApp/src/main/java/edu/egg/RecetApp/Servicios/IngredienteServicio.java
+++ RecetApp/src/main/java/edu/egg/RecetApp/Servicios/IngredienteServicio.java
@@ -1,10 +1,10 @@
-
 package edu.egg.RecetApp.Servicios;
 
 import edu.egg.RecetApp.Entidades.Foto;
 import edu.egg.RecetApp.Entidades.Ingrediente;
 import edu.egg.RecetApp.Errores.ErrorServicio;
 import edu.egg.RecetApp.Repositorios.IngredienteRepositorio;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -14,14 +14,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class IngredienteServicio {
-    
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Autowired
     private IngredienteRepositorio ingredienteRepositorio;
     @Autowired
     private FotoServicio fotoServicio;
-    
+
     @Transactional
-    public void nuevoIngrediente(String nombre, MultipartFile archivo) throws ErrorServicio{
+    public void nuevoIngrediente(String nombre, MultipartFile archivo) throws ErrorServicio {
         validar(nombre);
         Ingrediente ingrediente = new Ingrediente();
         ingrediente.setNombre(nombre);
@@ -29,10 +32,22 @@ public class IngredienteServicio {
         ingrediente.setFoto(foto);
         ingredienteRepositorio.save(ingrediente);
     }
+
     public void validar(String nombre) throws ErrorServicio {
         if (nombre == null || nombre.isEmpty()) {
             throw new ErrorServicio("El nombre del ingrediente no puede ser nulo.");
         }
-        
     }
+    
+    public Ingrediente buscarPorId(String id){
+        return em.find(Ingrediente.class, id);
+    }
+
+    public List<Ingrediente> buscarIngredientes(String q) {
+        return em.createQuery("SELECT c FROM Ingrediente c WHERE c.nombre LIKE :q").setParameter("q", "%" + q + "%").getResultList();
+    }
+    public List<Ingrediente> buscarIngredientes() {
+        return em.createQuery("SELECT c FROM Ingrediente c").getResultList();
+    }
+    
 }
